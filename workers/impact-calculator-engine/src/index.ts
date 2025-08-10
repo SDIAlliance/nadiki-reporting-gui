b297ab2d54dddf66f5bc43b94acf710f4731b305
Max Schulze <m@leitmotiv.digital>
2025-08-10 15:56:09 +0200

    boilerplate implementation of engine

--- workers/impact-calculator-engine/src/index.ts
+++ workers/impact-calculator-engine/src/index.ts
@@ -1,20 +1,20 @@
-import { DurableObject } from "cloudflare:workers";
+import { DurableObject, WorkerEntrypoint } from "cloudflare:workers";
+import { FacilityResponse } from 'registrar-api-client/types/facility-api'
 
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
+
+export class CalculatorService extends WorkerEntrypoint<Env> {
+	// starting point should be a timestamp
+	async createFacilityCalculator(
+		facilityId: string, config: FacilityResponse, startingPoint: number = 0
+	): Promise<void> {
+		const id = this.env.IMPACT_CALCULATOR_ENGINE.idFromName(`facility-${facilityId}`);
+		const stub = this.env.IMPACT_CALCULATOR_ENGINE.get(id);
+		await stub.startFacilityCalculator(facilityId, config, startingPoint)
+	}
+}
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject<Env> {
+export class CalculatorEngine extends DurableObject<Env> {
 	/**
 	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
 	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
@@ -24,6 +24,15 @@ export class MyDurableObject extends DurableObject<Env> {
 	 */
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
+	}
+
+	// starting point should be a timestamp
+	async startFacilityCalculator(
+		facilityId: string, config: FacilityResponse, startingPoint: number = 0
+	): Promise<void> {
+		await this.ctx.storage.put('entity', 'facility');
+		await this.ctx.storage.put('id', facilityId);
+		await this.ctx.storage.put('config', config);
 	}
 
 	/**
@@ -47,20 +56,20 @@ export default {
 	 * @param ctx - The execution context of the Worker
 	 * @returns The response to be sent back to the client
 	 */
-	async fetch(request, env, ctx): Promise<Response> {
-		// Create a `DurableObjectId` for an instance of the `MyDurableObject`
-		// class named "foo". Requests from all Workers to the instance named
-		// "foo" will go to a single globally unique Durable Object instance.
-		const id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName("foo");
+	// async fetch(request, env): Promise<Response> {
+	// 	// Create a `DurableObjectId` for an instance of the `MyDurableObject`
+	// 	// class named "foo". Requests from all Workers to the instance named
+	// 	// "foo" will go to a single globally unique Durable Object instance.
+	// 	const id: DurableObjectId = env.IMPACT_CALCULATOR_ENGINE.idFromName("foo");
 
-		// Create a stub to open a communication channel with the Durable
-		// Object instance.
-		const stub = env.MY_DURABLE_OBJECT.get(id);
+	// 	// Create a stub to open a communication channel with the Durable
+	// 	// Object instance.
+	// 	const stub = env.IMPACT_CALCULATOR_ENGINE.get(id);
 
-		// Call the `sayHello()` RPC method on the stub to invoke the method on
-		// the remote Durable Object instance
-		const greeting = await stub.sayHello("world");
+	// 	// Call the `sayHello()` RPC method on the stub to invoke the method on
+	// 	// the remote Durable Object instance
+	// 	const greeting = await stub.sayHello("world");
 
-		return new Response(greeting);
-	},
+	// 	return new Response(greeting);
+	// },
 } satisfies ExportedHandler<Env>;
